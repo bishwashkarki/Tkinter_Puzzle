5d281a40d167e5983b0248e2162efd8a90deba7c
bishwashkarki <93584579+bishwashkarki@users.noreply.github.com>
2021-11-30 13:31:18 -0100

    Update main_puzzle.py

--- image_puzzle/main_puzzle.py
+++ image_puzzle/main_puzzle.py
@@ -5,34 +5,53 @@ in this file iam creating a fiel that has 2x2 frames that can be moved around a 
 
 #imoprt modules
 import tkinter
+from tkinter.constants import INSIDE
 #from PIL import ImageTk, Image
 
 #tkinter settings
 win=tkinter.Tk()
 win.title("IMAGE PUZZLE")
-win.resizable(True, True)
+win.resizable(False, False)
 win.geometry("520x520")
 
 #checking if the button works
-def print_if_it_works():
-    X=250
-    Y=250
+def frame1_exchange():
+    #how to check postion
+    X, Y = frame4.winfo_rootx(), frame4.winfo_rooty()
+    X1, Y1 = frame1.winfo_rootx(), frame1.winfo_rooty()
     frame1.place(x=X,y=Y)
+    frame4.place(x=X1,y=Y1)
+
+def frame2_exchange():
+    #how to check postion
+    X, Y = frame4.winfo_rootx(), frame4.winfo_rooty()
+    X1, Y1 = frame2.winfo_rootx(), frame2.winfo_rooty()
+    frame1.place(x=X,y=Y)
+    frame4.place(x=X1,y=Y1)
+
+def frame3_exchange():
+    #how to check postion
+    X, Y = frame4.winfo_rootx(), frame4.winfo_rooty()
+    X1, Y1 = frame3.winfo_rootx(), frame3.winfo_rooty()
+    frame1.place(x=X,y=Y)
+    frame4.place(x=X1,y=Y1)
+
+
 
 
 #creating a frame
 #postion=x=700,y=100
 frame1=tkinter.LabelFrame(win)
-frame1.place(x=0,y=0)
+frame1.place(x=0,y=0,bordermode=INSIDE,height=260, width=260)
 # 2 
 frame2=tkinter.LabelFrame(win)
-frame2.place(x=0,y=250)
+frame2.place(x=0,y=250,bordermode=INSIDE,height=260, width=260)
 # 3
 frame3=tkinter.LabelFrame(win)
-frame3.place(x=250,y=0)
+frame3.place(x=250,y=0,bordermode=INSIDE,height=260, width=260)
 # 4
 frame4=tkinter.LabelFrame(win)
-frame4.place(x=250,y=250)
+frame4.place(x=250,y=250,bordermode=INSIDE,height=260, width=260)
 
 #image_size= 500x500
 """
@@ -47,20 +66,17 @@ image4=tkinter.PhotoImage(file="images/GIF/main_image_bottom_right.gif")
 #running=True
 #while running:
 #button1
-button1= tkinter.Button(frame1, image=image1, command=print_if_it_works)
+button1= tkinter.Button(frame1, image=image1, command=frame1_exchange)
 button1.grid()
 #button2
-button1= tkinter.Button(frame2, image=image2, command=print_if_it_works)
-button1.grid()
+button2= tkinter.Button(frame2, image=image2, command=frame2_exchange)
+button2.grid()
 #button3
-button1= tkinter.Button(frame3, image=image3, command=print_if_it_works)
-button1.grid()
+button3= tkinter.Button(frame3, image=image3, command=frame3_exchange)
+button3.grid()
 #button4
-#button1= tkinter.Button(frame4, image=image4, command=print_if_it_works)
-#button1.place()
-
-
-
+button4= tkinter.Button(frame4, image=image4)
+button4.grid()
 
 
 
