97ffa684447c13050726c85ac2c827245a2d373e
bishwashkarki <93584579+bishwashkarki@users.noreply.github.com>
2021-11-30 09:05:06 -0100

    Update main_puzzle.py

--- image_puzzle/main_puzzle.py
+++ image_puzzle/main_puzzle.py
@@ -5,13 +5,13 @@ in this file iam creating a fiel that has 2x2 frames that can be moved around a 
 
 #imoprt modules
 import tkinter
-from PIL import ImageTk, Image
+#from PIL import ImageTk, Image
 
 #tkinter settings
 win=tkinter.Tk()
 win.title("IMAGE PUZZLE")
-win.resizable(False, False)
-win.geometry("500x500")
+win.resizable(True, True)
+win.geometry("520x520")
 
 #checking if the button works
 def print_if_it_works():
@@ -21,39 +21,41 @@ def print_if_it_works():
 #creating a frame
 #postion=x=700,y=100
 frame1=tkinter.LabelFrame(win)
-frame1.grid(row=1,column=0)
+frame1.place(x=0,y=0)
 # 2 
 frame2=tkinter.LabelFrame(win)
-frame2.grid(row=1,column=0)
+frame2.place(x=0,y=250)
 # 3
 frame3=tkinter.LabelFrame(win)
-frame3.grid(row=0,column=1)
+frame3.place(x=250,y=0)
 # 4
 frame4=tkinter.LabelFrame(win)
-frame4.grid(row=1,column=1)
+frame4.place(x=250,y=250)
 
-#image_size= 3126x5000
-pic=Image.open("images/front_image1.gif")
+#image_size= 500x500
+"""
+background=tkinter.PhotoImage(file="main_image.jpg")
+"""
+#setting images
+image1=tkinter.PhotoImage(file="images/GIF/main_image_top_left.gif")
+image2=tkinter.PhotoImage(file="images/GIF/main_image_top_right.gif")
+image3=tkinter.PhotoImage(file="images/GIF/main_image_bottm_left.gif")
+image4=tkinter.PhotoImage(file="images/GIF/main_image_bottom_right.gif")
 
-#resizing the pic 
-resized_pic=pic.resize((250,250), Image.ANTIALIAS)
-
-running=True
-while running:
-    #setting up image to frame
-    image1= ImageTk.PhotoImage(resized_pic)
-    #button1
-    button1= tkinter.Button(frame1, image=image1, command=print_if_it_works)
-    button1.grid()
-    #button2
-    button1= tkinter.Button(frame2, image=image1, command=print_if_it_works)
-    button1.grid()
-    #button3
-    button1= tkinter.Button(frame3, image=image1, command=print_if_it_works)
-    button1.grid()
-    #button4
-    #button1= tkinter.Button(frame4, image=image1, command=print_if_it_works)
-    #button1.grid()
+#running=True
+#while running:
+#button1
+button1= tkinter.Button(frame1, image=image1, command=print_if_it_works)
+button1.place()
+#button2
+button1= tkinter.Button(frame2, image=image2, command=print_if_it_works)
+button1.place()
+#button3
+button1= tkinter.Button(frame3, image=image3, command=print_if_it_works)
+button1.place()
+#button4
+#button1= tkinter.Button(frame4, image=image4, command=print_if_it_works)
+#button1.place()
 
 
 
